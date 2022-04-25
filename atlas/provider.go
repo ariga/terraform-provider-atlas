3364895548cac4879545236e1e954997f775f650
Amit Shani <amit.shani1995@gmail.com>
2022-04-25 17:47:17 +0300

    oops

--- atlas/provider.go
+++ atlas/provider.go
@@ -8,7 +8,7 @@ func Provider() *schema.Provider {
 	return &schema.Provider{
 		Schema: map[string]*schema.Schema{},
 		ResourcesMap: map[string]*schema.Resource{
-			"atlas_schema": newSchemaDatasource(),
+			"atlas_schema": newSchemaResource(),
 		},
 		DataSourcesMap: map[string]*schema.Resource{
 			"atlas_schema": newSchemaDatasource(),
