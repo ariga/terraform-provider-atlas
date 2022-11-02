ea7b6e926c8c28dd0a40bf36ff767bb7b02e3ed0
Giau. Tran Minh <hello@giautm.dev>
2022-11-03 00:02:03 +0700

    feat: added new DS for read migration status
    Signed-off-by: Giau. Tran Minh <hello@giautm.dev>

---
+++ internal/provider/atlas_migration_data_source.go
@@ -0,0 +1,152 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+
+	"ariga.io/ariga/terraform-provider-atlas/internal/atlas"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+type (
+	// MigrationDataSource defines the data source implementation.
+	MigrationDataSource struct {
+		client *atlas.MigrateClient
+	}
+	// MigrationDataSourceModel describes the data source data model.
+	MigrationDataSourceModel struct {
+		DirURL          types.String `tfsdk:"dir"`
+		URL             types.String `tfsdk:"url"`
+		RevisionsSchema types.String `tfsdk:"revisions_schema"`
+
+		Status  types.String `tfsdk:"status"`
+		Current types.String `tfsdk:"current"`
+		Next    types.String `tfsdk:"next"`
+		Latest  types.String `tfsdk:"latest"`
+		ID      types.String `tfsdk:"id"`
+	}
+)
+
+// Ensure provider defined types fully satisfy framework interfaces
+var (
+	_ datasource.DataSource              = &MigrationDataSource{}
+	_ datasource.DataSourceWithConfigure = &MigrationDataSource{}
+)
+var (
+	latestVersion = "Already at latest version"
+	noMigration   = "No migration applied yet"
+)
+
+// NewMigrationDataSource returns a new AtlasSchemaDataSource.
+func NewMigrationDataSource() datasource.DataSource {
+	return &MigrationDataSource{}
+}
+
+// Metadata implements datasource.DataSource.
+func (d *MigrationDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_migration"
+}
+
+// Configure implements datasource.DataSourceWithConfigure.
+func (d *MigrationDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	// Prevent panic if the provider has not been configured.
+	if req.ProviderData == nil {
+		return
+	}
+	c, ok := req.ProviderData.(*atlas.MigrateClient)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected *atlas.MigrateClient, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+		return
+	}
+	d.client = c
+}
+
+// GetSchema implements datasource.DataSource.
+func (d *MigrationDataSource) GetSchema(ctx context.Context) (tfsdk.Schema, diag.Diagnostics) {
+	return tfsdk.Schema{
+		Description: "Data source returns the information about the current migration.",
+		Attributes: map[string]tfsdk.Attribute{
+			"url": {
+				Description: "[driver://username:password@address/dbname?param=value] select a resource using the URL format",
+				Type:        types.StringType,
+				Required:    true,
+				Sensitive:   true,
+			},
+			"dir": {
+				Description: "Select migration directory using URL format",
+				Type:        types.StringType,
+				Required:    true,
+			},
+			"revisions_schema": {
+				Description: "The name of the schema the revisions table resides in",
+				Type:        types.StringType,
+				Optional:    true,
+			},
+			"status": {
+				Description: "The Status of migration (OK, PENDING)",
+				Type:        types.StringType,
+				Computed:    true,
+			},
+			"current": {
+				Description: "Current migration version",
+				Type:        types.StringType,
+				Computed:    true,
+			},
+			"next": {
+				Description: "Next migration version",
+				Type:        types.StringType,
+				Computed:    true,
+			},
+			"latest": {
+				Description: "The latest version of the migration is in the migration directory",
+				Type:        types.StringType,
+				Computed:    true,
+			},
+			"id": {
+				Description: "The ID of the migration",
+				Type:        types.StringType,
+				Computed:    true,
+			},
+		},
+	}, nil
+}
+
+// Read implements datasource.DataSource.
+func (d *MigrationDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var data MigrationDataSourceModel
+	// Read Terraform configuration data into the model
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	r, err := d.client.Status(ctx, &atlas.StatusParams{
+		DirURL:          data.DirURL.Value,
+		URL:             data.URL.Value,
+		RevisionsSchema: data.RevisionsSchema.Value,
+	})
+	if err != nil {
+		resp.Diagnostics.Append(atlas.ErrorDiagnostic(err, "Failed to read migration status"))
+		return
+	}
+	data.Status = types.String{Value: r.Status}
+	if r.Status == "PENDING" && r.Current == noMigration {
+		data.Current = types.String{Null: true}
+	} else {
+		data.Current = types.String{Value: r.Current}
+	}
+	if r.Status == "OK" && r.Next == latestVersion {
+		data.Next = types.String{Null: true}
+	} else {
+		data.Next = types.String{Value: r.Next}
+	}
+	v := r.LatestVersion()
+	data.ID = data.DirURL
+	data.Latest = types.String{Value: v, Null: v == ""}
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
---
+++ internal/provider/atlas_migration_data_source_test.go
@@ -0,0 +1,51 @@
+package provider_test
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccMigrationDataSource(t *testing.T) {
+	schema := "test"
+	tempSchemas(t, mysqlURL, schema)
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+				data "atlas_migration" "hello" {
+					dir = "migrations?format=atlas"
+					url = "%s/%s"
+				}
+				`, mysqlURL, schema),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.atlas_migration.hello", "id", "migrations?format=atlas"),
+					resource.TestCheckResourceAttr("data.atlas_migration.hello", "status", "PENDING"),
+					resource.TestCheckNoResourceAttr("data.atlas_migration.hello", "current"),
+					resource.TestCheckResourceAttr("data.atlas_migration.hello", "next", "20221101163823"),
+					resource.TestCheckResourceAttr("data.atlas_migration.hello", "latest", "20221101165415"),
+				),
+			},
+		},
+	})
+	// Invalid hash
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+				data "atlas_migration" "hello" {
+					dir = "migrations-hash?format=atlas"
+					url = "%s/%s"
+				}
+				`, mysqlURL, schema),
+				ExpectError: regexp.MustCompile("You have a checksum error in your migration directory"),
+			},
+		},
+	})
+}
