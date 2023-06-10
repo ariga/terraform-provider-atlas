826be10b58e6e658b6939cbc06af0f9d65f338b4
Giau. Tran Minh <hello@giautm.dev>
2023-06-11 05:36:29 +0700

    migration: added test for data source

--- internal/provider/atlas_migration_data_source_test.go
+++ internal/provider/atlas_migration_data_source_test.go
@@ -1,11 +1,19 @@
 package provider_test
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"regexp"
+	"strings"
 	"testing"
 
+	"ariga.io/atlas/sql/migrate"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/stretchr/testify/require"
 )
 
 func TestAccMigrationDataSource(t *testing.T) {
@@ -48,4 +56,94 @@ func TestAccMigrationDataSource(t *testing.T) {
 			},
 		},
 	})
+}
+
+func TestAccMigrationDataSource_RemoteDir(t *testing.T) {
+	var (
+		dir migrate.MemDir
+		srv = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			var m struct {
+				Query     string `json:"query"`
+				Variables struct {
+					Input json.RawMessage `json:"input"`
+				} `json:"variables"`
+			}
+			require.NoError(t, json.NewDecoder(r.Body).Decode(&m))
+			switch {
+			case strings.Contains(m.Query, "query"):
+				writeDir(t, &dir, w)
+			default:
+				t.Fatalf("unexpected query: %s", m.Query)
+			}
+		}))
+		config = fmt.Sprintf(`
+data "atlas_migration" "hello" {
+	url = "%s/test"
+	remote_dir {
+		name = "test"
+	}
+	cloud {
+		token = "aci_bearer_token"
+		url   = "%s"
+	}
+}`, mysqlURL, srv.URL)
+	)
+	t.Cleanup(srv.Close)
+
+	t.Run("NoPendingFiles", func(t *testing.T) {
+		tempSchemas(t, mysqlURL, "test")
+		resource.Test(t, resource.TestCase{
+			PreCheck:                 func() { testAccPreCheck(t) },
+			ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+			Steps: []resource.TestStep{
+				{
+					Config: config,
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr("data.atlas_migration.hello", "id", "remote_dir://test"),
+						resource.TestCheckResourceAttr("data.atlas_migration.hello", "status", "OK"),
+						resource.TestCheckResourceAttr("data.atlas_migration.hello", "current", "No migration applied yet"),
+						resource.TestCheckResourceAttr("data.atlas_migration.hello", "next", ""),
+						resource.TestCheckNoResourceAttr("data.atlas_migration.hello", "latest"),
+					),
+				},
+			},
+		})
+	})
+
+	t.Run("WithFiles", func(t *testing.T) {
+		require.NoError(t, dir.WriteFile("1.sql", []byte("create table foo (id int)")))
+		require.NoError(t, dir.WriteFile("2.sql", []byte("create table bar (id int)")))
+
+		tempSchemas(t, mysqlURL, "test")
+		resource.Test(t, resource.TestCase{
+			PreCheck:                 func() { testAccPreCheck(t) },
+			ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+			Steps: []resource.TestStep{
+				{
+					Config: config,
+					Check: resource.ComposeAggregateTestCheckFunc(
+						resource.TestCheckResourceAttr("data.atlas_migration.hello", "id", "remote_dir://test"),
+						resource.TestCheckResourceAttr("data.atlas_migration.hello", "status", "PENDING"),
+						resource.TestCheckResourceAttr("data.atlas_migration.hello", "current", ""),
+						resource.TestCheckResourceAttr("data.atlas_migration.hello", "next", "1"),
+						resource.TestCheckResourceAttr("data.atlas_migration.hello", "latest", "2"),
+					),
+				},
+			},
+		})
+	})
+}
+
+func writeDir(t *testing.T, dir migrate.Dir, w io.Writer) {
+	// Checksum before archiving.
+	hf, err := dir.Checksum()
+	require.NoError(t, err)
+	ht, err := hf.MarshalText()
+	require.NoError(t, err)
+	require.NoError(t, dir.WriteFile(migrate.HashFileName, ht))
+	// Archive and send.
+	arc, err := migrate.ArchiveDir(dir)
+	require.NoError(t, err)
+	_, err = fmt.Fprintf(w, `{"data":{"dir":{"content":%q}}}`, base64.StdEncoding.EncodeToString(arc))
+	require.NoError(t, err)
 }
