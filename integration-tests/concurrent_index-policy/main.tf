bfe853a9bcf21a8c6ef7e07c0b527fc6f6fe4c57
Giau. Tran Minh <hello@giautm.dev>
2023-11-27 00:53:10 +0700

    chore: fixed urls

--- integration-tests/concurrent_index-policy/main.tf
+++ integration-tests/concurrent_index-policy/main.tf
@@ -14,13 +14,13 @@ variable "schema" {
 
 data "atlas_schema" "db" {
   src     = "file://${var.schema}"
-  dev_url = "postgres://postgres:pass@localhost:5433/"
+  dev_url = "postgres://postgres:pass@localhost:5433/test?sslmode=disable"
 }
 
 resource "atlas_schema" "db" {
   hcl     = data.atlas_schema.db.hcl
-  url     = "postgres://postgres:pass@localhost:5432/"
-  dev_url = "postgres://postgres:pass@localhost:5433/"
+  url     = "postgres://postgres:pass@localhost:5432/test?sslmode=disable"
+  dev_url = "postgres://postgres:pass@localhost:5433/test?sslmode=disable"
   diff {
     concurrent_index {
       create = true
