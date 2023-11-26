e325d911c9acf3cad2903ae15f168a35ce8d51c6
Giau. Tran Minh <hello@giautm.dev>
2023-11-26 19:05:43 +0700

    chore: added atlas_schema template

--- internal/provider/template.go
+++ internal/provider/template.go
@@ -2,7 +2,11 @@ package provider
 
 import (
 	"embed"
+	"errors"
+	"fmt"
+	"io"
 	"os"
+	"strings"
 	"text/template"
 )
 
@@ -29,12 +33,40 @@ type (
 		Baseline        string
 		RevisionsSchema string
 	}
+	schemaData struct {
+		Source  string
+		URL     string
+		DevURL  string
+		Schemas []string
+		Exclude []string
+		Diff    *Diff
+	}
 )
 
 var (
 	//go:embed templates/*.tmpl
 	tmpls embed.FS
 	tmpl  = template.Must(template.New("terraform").
+		Funcs(template.FuncMap{
+			"hclValue": func(s string) string {
+				if s == "" {
+					return s
+				}
+				return strings.ReplaceAll(strings.ToUpper(s), "-", "_")
+			},
+			"slides": func(s []string) (string, error) {
+				b := &strings.Builder{}
+				b.WriteRune('[')
+				for i, v := range s {
+					if i > 0 {
+						b.WriteRune(',')
+					}
+					fmt.Fprintf(b, "%q", v)
+				}
+				b.WriteRune(']')
+				return b.String(), nil
+			},
+		}).
 		ParseFS(tmpls, "templates/*.tmpl"),
 	)
 )
@@ -48,4 +80,18 @@ func (d *templateData) CreateFile(name string) error {
 	}
 	defer f.Close()
 	return tmpl.ExecuteTemplate(f, "atlas_migration.tmpl", d)
+}
+
+// render renders the atlas.hcl template.
+//
+// The template is used by the Atlas CLI to apply the schema.
+// It also validates the data before rendering the template.
+func (d *schemaData) render(w io.Writer) error {
+	// if d.EnvName == "" {
+	// 	return errors.New("env name is not set")
+	// }
+	if d.URL == "" {
+		return errors.New("database url is not set")
+	}
+	return tmpl.ExecuteTemplate(w, "atlas_schema.tmpl", d)
 }
--- internal/provider/template_test.go
+++ internal/provider/template_test.go
@@ -1,6 +1,7 @@
 package provider
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -86,4 +87,37 @@ func checkContent(t *testing.T, actual string, gen func(string) error) {
 	a, err := os.ReadFile(actual)
 	require.NoError(t, err)
 	require.Equal(t, string(e), string(a))
+}
+
+func Test_SchemaTemplate(t *testing.T) {
+	data := &schemaData{
+		Source: "file://schema.hcl",
+		URL:    "mysql://user:pass@localhost:3306/tf-db",
+		DevURL: "mysql://user:pass@localhost:3307/tf-db",
+		Diff: &Diff{
+			Skip: &SkipChanges{
+				AddIndex:  true,
+				DropTable: true,
+			},
+		},
+	}
+
+	out := &bytes.Buffer{}
+	require.NoError(t, data.render(out))
+	require.Equal(t, `
+diff {
+  skip {
+    drop_table = true
+    add_index = true
+  }
+}
+env {
+  name = atlas.env
+  src  = "file://schema.hcl"
+  url  = "mysql://user:pass@localhost:3306/tf-db"
+  dev  = "mysql://user:pass@localhost:3307/tf-db"
+  schemas = []
+  exclude = []
+}
+`, out.String())
 }
