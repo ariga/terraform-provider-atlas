c3dd9d92ec2cdf64622d772a8bb0c7ba22cbb074
Giau. Tran Minh <hello@giautm.dev>
2024-07-25 20:56:12 +0700

    chore: cleanup

--- internal/provider/builder.go
+++ internal/provider/builder.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
 	"github.com/hashicorp/hcl/v2/hclwrite"
 	"github.com/zclconf/go-cty/cty"
 )
@@ -156,4 +157,67 @@ func hclValue(s string) string {
 		return ""
 	}
 	return strings.ReplaceAll(strings.ToUpper(s), "-", "_")
+}
+
+func parseConfig(cfg string) (*hclwrite.File, error) {
+	f, diags := hclwrite.ParseConfig([]byte(cfg), "atlas.hcl", hcl.InitialPos)
+	if diags.HasErrors() {
+		return nil, diags
+	}
+	return f, nil
+}
+
+func mergeFile(dst, src *hclwrite.File) {
+	dstBody, srcBody := dst.Body(), src.Body()
+	dstBlocks := make(map[string]*hclwrite.Block)
+	for _, blk := range dstBody.Blocks() {
+		dstBlocks[address(blk)] = blk
+	}
+	for _, blk := range srcBody.Blocks() {
+		if dstBlk, ok := dstBlocks[address(blk)]; ok {
+			// Merge the blocks if they have the same address.
+			mergeBlock(dstBlk, blk)
+		} else {
+			appendBlock(dstBody, blk)
+		}
+	}
+}
+
+func address(block *hclwrite.Block) string {
+	parts := append([]string{block.Type()}, block.Labels()...)
+	return strings.Join(parts, ".")
+}
+
+func mergeBlock(dst, src *hclwrite.Block) {
+	dstBody, srcBody := dst.Body(), src.Body()
+	for name, attr := range srcBody.Attributes() {
+		dstBody.SetAttributeRaw(name, attr.Expr().BuildTokens(nil))
+	}
+	srcBlocks := srcBody.Blocks()
+	srcBlockTypes := make(map[string]struct{})
+	for _, blk := range srcBlocks {
+		srcBlockTypes[blk.Type()] = struct{}{}
+	}
+	for _, blk := range dstBody.Blocks() {
+		if _, conflict := srcBlockTypes[blk.Type()]; conflict {
+			// Remove the block from the destination if it already exists.
+			dstBody.RemoveBlock(blk)
+		}
+	}
+	for _, blk := range srcBlocks {
+		appendBlock(dstBody, blk)
+	}
+}
+
+// appendBlock appends a block to the body and ensures there is a newline before the block.
+// It returns the appended block.
+//
+// There is a bug in hclwrite that causes the block to be appended without a newline
+// https://github.com/hashicorp/hcl/issues/687
+func appendBlock(body *hclwrite.Body, blk *hclwrite.Block) *hclwrite.Block {
+	t := body.BuildTokens(nil)
+	if len(t) == 0 || t[len(t)-1].Type != hclsyntax.TokenNewline {
+		body.AppendNewline()
+	}
+	return body.AppendBlock(blk)
 }
--- internal/provider/builder_test.go
+++ internal/provider/builder_test.go
@@ -115,6 +115,45 @@ func Test_SchemaTemplate(t *testing.T) {
 }`, out.String())
 }
 
+func Test_mergeFile(t *testing.T) {
+	dst, err := parseConfig(`
+atlas {}
+env {
+  name = atlas.env
+}
+`)
+	require.NoError(t, err)
+
+	src, err := parseConfig(`
+atlas {
+	cloud {
+  	token = "aci_token"
+  }
+}
+env {
+  migration {
+		dir = "file://migrations"
+	}
+}
+`)
+	require.NoError(t, err)
+	mergeFile(dst, src)
+
+	require.Equal(t, `
+atlas {
+  cloud {
+    token = "aci_token"
+  }
+}
+env {
+  name = atlas.env
+  migration {
+    dir = "file://migrations"
+  }
+}
+`, string(dst.Bytes()))
+}
+
 func checkContent(t *testing.T, actual string, gen func(string) error) {
 	t.Helper()
 	expected := filepath.Join(".", "testdata", fmt.Sprintf("%s-cfg.hcl", t.Name()))
