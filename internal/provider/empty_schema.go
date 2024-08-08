544469e136d869433592f6dec4af77b9b279c911
Giau. Tran Minh <hello@giautm.dev>
2024-08-08 16:09:05 +0700

    bleh

--- internal/provider/empty_schema.go
+++ internal/provider/empty_schema.go
@@ -12,14 +12,21 @@ func emptySchemas(schema string) (string, error) {
 		return "", diags
 	}
 	root := f.Body()
+	schemas := 0
 	for _, blk := range root.Blocks() {
 		if blk.Type() == "schema" {
+			schemas++
 			// Clear the schema block to get an empty schema.
 			blk.Body().Clear()
 		} else {
 			// Remove all other blocks.
 			root.RemoveBlock(blk)
 		}
+	}
+	if schemas > 1 {
+		// If there are more than one schema blocks, return an empty string.
+		// To clear the whole realm
+		return "", nil
 	}
 	return string(f.Bytes()), nil
 }
